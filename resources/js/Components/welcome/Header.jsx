f896ee6e44487e43e689c897428a7a9c57919191
Jhaseft <jhasesaat@gmail.com>
2025-11-13 08:33:26 -0400

    changes

--- resources/js/Components/welcome/Header.jsx
+++ resources/js/Components/welcome/Header.jsx
@@ -4,7 +4,7 @@ export default function Header({ auth }) {
     return (
         <header className="w-full bg-white dark:bg-zinc-800 shadow-md py-4">
             <div className="container mx-auto flex justify-between items-center px-6">
-                <h1 className="text-2xl font-bold text-[#FF2D20]">Saat Shop 🛒</h1>
+                <h1 className="text-2xl font-bold text-[#FF2D20]">Saat Shop </h1>
                 <nav className="flex gap-4">
                     {auth?.user ? (
                         <Link
