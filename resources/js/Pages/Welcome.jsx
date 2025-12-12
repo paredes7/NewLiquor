905c9f49bd272a47edf0caa30232f276808b97a4
Jhaseft <jhasesaat@gmail.com>
2025-12-12 07:51:03 -0400

    fix error title

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,6 +1,6 @@
 import Products from '@/Components/welcome/Products';
 import Layout from '@/Layouts/MainLayout';
-import Videos from '@/Components/welcome/videos';
+import Videos from '@/Components/welcome/Videos';
 export default function Welcome({ auth }) {
     return (
         <Layout title="Exclusive|Inicio" auth={auth}>
